bac859150e33fe47c9ab781b3bbe6e9a21fdf07f
L-Forster <louisforster208@gmail.com>
2023-10-29 16:10:51 +0000

    Update Friendly.cs
    Improved Move Method.

--- Assets/Units/Friendly.cs
+++ Assets/Units/Friendly.cs
@@ -9,8 +9,12 @@ public class Friendly : Unit
     // Start is called before the first frame update
 	private Vector2 position;
 	public void Move(Vector2 target_pos){
-		position.x = position.x + target_pos.x;
-		position.y = position.y + target_pos.y;
+		position.x = position.x - target_pos.x;
+		position.y = position.y - target_pos.y;
+		Vector2 tempPos = new Vector2(position.x, position.y);
+		tempPos = tempPos.normalized;
+		tempPos = tempPos * 5;
+		transform.position = tempPos;
 	}
 
 	bool selected
@@ -31,12 +35,23 @@ public class Friendly : Unit
         mousePos.z = -Camera.main.transform.position.z; // Set the z-coordinate to match the camera's position
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(mousePos);
      
+		// if right click move the unit
+		if (selected)
+		{
+			if(Input.GetMouseButtonDown(1)){
+				Move(mousePosition);
+			}
+			selected = false;
+			// Remove highlight
+		}
+		
+	// check if you have selected it
+		Select();
+
         // if the target is on a unit, highlight them
-     
-        
         return mousePosition;
     }
-
+	
     void Select()
     {
 		float difference = Vector2.Distance(onMouseDown(), position);
@@ -45,6 +60,8 @@ public class Friendly : Unit
 			selected = true;
 		}
         // insert code to highlight the thing
+
+
     }
 	void HighlightSelection(){
 		
@@ -59,16 +76,9 @@ public class Friendly : Unit
     // Update is called once per frame
     void Update()
     {
-     if (selected)
-	{
-    	position = onMouseDown();
-		Move(position);
-	}
-        
-}
-
 
 
 	
+	}	
 }
 
