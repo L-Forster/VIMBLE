cc26863fecb61edc227b7ccf12b23e8fc131e057
Mert <xk21576@bristol.ac.uk>
2023-10-29 12:56:54 +0000

    Added Building class and battery

--- Assets/Battery.cs
+++ Assets/Battery.cs
@@ -2,27 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Building : MonoBehaviour
+public class Battery : Building
 {
-
-public struct Location{
-    int x;
-    int y;
-}
-
-public string buildingType;
-
-public int buildTime;
-
-public int hp;
-
-public int[][] cost;
-
-public Location location;
-
-public Location[] buildSpace;
-
-
+   
     // Start is called before the first frame update
     void Start()
     {
