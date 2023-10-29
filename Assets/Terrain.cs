ae5081860860321cdcd14ae012d9c5dc8bec88a3
N4mFlashback <ay22840@bristol.ac.uk>
2023-10-29 14:15:48 +0000

    Update Terrain.cs

--- Assets/Terrain.cs
+++ Assets/Terrain.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
 
-public class Terrain : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class resource : MonoBehaviour
+{ 
+    resource()
     {
-        
     }
 
+}
+public class Terrain : MonoBehaviour
+{
+    public resource Resource;
+    public bool foliage;
+    // Start is called before the first frame update
+
     // Update is called once per frame
-    void Update()
+    void removeFoliage()
     {
-        
+        //remove foliage
+        foliage = false;
     }
 }
+
+public class water : Terrain
+{
+    
+}
+public class flatlands : Terrain
+{
+    
+}
+
+public class mountains : Terrain
+{
+    private new Collider2D Collider2D;
+}
+
