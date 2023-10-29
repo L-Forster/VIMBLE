90de34d623234d6fd8d45c30a39feb7f48cd8d7b
L-Forster <louisforster208@gmail.com>
2023-10-29 22:56:48 +0000

    Fixed syntax errors

--- Assets/abandoned house.cs
+++ Assets/abandoned house.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class abandonedhouse : building
+public class abandonedhouse : Building
 {
     public bool broke = false;
     int Timeremaining = 10;
@@ -17,9 +17,11 @@ public class abandonedhouse : building
     {
         if (broke && Timeremaining > 0)
         {
-            Timeremaining -= 1
-        }else if (Timeremaining == 0 && broke){
-            destroy(this.building);
+            Timeremaining -= 1;
+        }
+        else if (Timeremaining == 0 && broke)
+        {
+            Destroy(this);
         }
     }
 }
