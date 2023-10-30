c43a0d5803678b3c34fa74784233968a797d9b5f
L-Forster <120142008+L-Forster@users.noreply.github.com>
2023-10-30 12:26:16 +0000

    Update mine.cs

--- Assets/Scripts/mine.cs
+++ Assets/Scripts/mine.cs
@@ -1,10 +1,17 @@
+using UnityEngine;
+
+using UnityEngine.UI;
+
 public class Mine : Building
 {
     private int metal = 0;
-
+    public TextMesh health_text;
+//
     // Start is called before the first frame update
     void Start()
     {
+        health_text = GetComponentInChildren<TextMesh>(); // Assuming the Text component is a child of the unit's GameObject		isMoving = false;
+
         buildingType = "Mine";
         maxHp = 20;
         buildTime = 10;
@@ -30,6 +37,12 @@ public class Mine : Building
     // Update is called once per frame
     void Update()
     {
+        health_text.text = health.ToString() ;
+
+        if(health <= 0){
+            gameObject.SetActive(false);
+            Destroy(this);
+        }
     //    int energyFromBattery = Building.Battery.energy;
      //   MineMetal(ref energyFromBattery);
 
