89cb5da9d18f39d03cac712285b8d0580e6cd1a9
L-Forster <louisforster208@gmail.com>
2023-10-30 13:04:15 +0000

    s
    ss

--- Assets/Units/IUnit.cs
+++ Assets/Units/IUnit.cs
@@ -9,13 +9,9 @@ namespace IUnitNamespace{
         int health { get; set; }
         int speed { get; set; }
         int damage { get; set; }
-        Vector2 position { get; set; }
         bool alive { get; set; }
 
-        Unit target { get; set; }
 
-        //methods
-        void DoDamage(Unit target);
 
     }
 }
