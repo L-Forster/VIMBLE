8ce128c090a8e4b178c262f5ce13bc003eb39963
N4mFlashback <ay22840@bristol.ac.uk>
2023-10-30 10:46:44 +0000

    tree tile working with interatction with water

---
+++ Assets/Tree.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class Tree : MonoBehaviour
+{
+    public int health;
+    public Tilemap Tilemap;
+
+    public List<TileBase> tiles;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (tiles.Contains(Tilemap.GetTile(Vector3Int.FloorToInt(this.transform.localPosition))))
+        {
+            Destroy(this);
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (health <= 0)
+        {
+            Destroy(this);
+        }
+    }
+}
