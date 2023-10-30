2115d4c4d4152e754a2ddbd04e467360a2148a2a
L-Forster <120142008+L-Forster@users.noreply.github.com>
2023-10-30 12:40:09 +0000

    Update Tree.cs

--- Assets/Tree.cs
+++ Assets/Tree.cs
@@ -1,30 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
 public class Tree : MonoBehaviour
 {
-    public int health;
-    public Tilemap Tilemap;
-
-    public List<TileBase> tiles;
     // Start is called before the first frame update
-    void Start()
-    {
-        if (tiles.Contains(Tilemap.GetTile(Vector3Int.FloorToInt(this.transform.localPosition))))
-        {
-            Destroy(this);
-        }
+    public int health;
+    public bool alive;
+    public TextMesh health_text;
 
+    void Start()
+    
+    {
+        health_text = GetComponentInChildren<TextMesh>(); // Assuming the Text component is a child of the unit's GameObject		isMoving = false;
+   
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if (health <= 0)
+        health_text.text = health.ToString() ;
+
+        if (!alive)
         {
             gameObject.SetActive(false);
-
             Destroy(this);
         }
     }
